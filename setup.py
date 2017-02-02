1dd2d3686f50a3af03bb547f9c3b043e218e0ff0
R S Nikhil Krishna <rsnk96@gmail.com>
2017-02-02 11:12:58 +0530

    started conversion to console script

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from distutils.core import setup
-import py2exe, os
+import os
 
-# setup(console=['org_e.py'])
-Mydata_files = [('images', [os.path.join('images','Org-E bg.PNG'), os.path.join('images','Org-E Logo.png')])]
-setup(
-    # options = {'py2exe': {'includes':['sip', 'PyQt4.QtCore', 'PyQt4.QtGui']}},
-    options = {'py2exe': {'includes':['sip'], 'bundle_files': 1, 'compressed': True}},
-    scripts = ['shell_script.sh'],
-    data_files = Mydata_files,
-    windows = [{'script': "org_e.py", 'icon_resources': [(0, os.path.join('images','Org-E_Logo.ico'))]}],
-    zipfile = None,
-)
+setup(console=['org_e.py'])
+
