ac9031c33f321d3d734bf8cfe9ddeeedf185f0af
R S Nikhil Krishna <rsnk96@gmail.com>
2017-02-02 11:06:56 +0530

    This version works on windows creating an application

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ Mydata_files = [('images', [os.path.join('images','Org-E bg.PNG'), os.path.join(
 setup(
     # options = {'py2exe': {'includes':['sip', 'PyQt4.QtCore', 'PyQt4.QtGui']}},
     options = {'py2exe': {'includes':['sip'], 'bundle_files': 1, 'compressed': True}},
+    scripts = ['shell_script.sh'],
     data_files = Mydata_files,
     windows = [{'script': "org_e.py", 'icon_resources': [(0, os.path.join('images','Org-E_Logo.ico'))]}],
     zipfile = None,
